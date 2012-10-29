9827bd5462192e83c22d88efcfc7a9715584b511
Jeff Iacono <iacono@squareup.com>
2012-10-29 00:43:44 -0700

    Add example to header comments

--- arith.R
+++ arith.R
@@ -3,6 +3,17 @@
 # Usage:
 #
 #   $ Rscript path/to/arith.R arg1 arg2
+#
+# Example:
+#
+#   $ Rscript path/to/arith.R 10 5
+#   x= 10
+#   y= 5
+#   x + y = 15
+#   x - y = 5
+#   x * y = 50
+#   x / y = 2
+#
 argv <- commandArgs(TRUE)
 
 x <- as.numeric(argv[1])
